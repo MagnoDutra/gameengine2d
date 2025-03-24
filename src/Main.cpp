8d1d74f981efdf2da11292fdfd1160570dd4c5e0
Magno <jmagno.dutra@hotmail.com>
2025-03-24 17:38:30 -0300

    Game class skeleton created

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <sol/sol.hpp>
+#include "Game.h"
 
-int main() {
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
+int main(int argc, char* argv[]) {
+    Game game;
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+    game.Initialize();
+    game.Run();
+    game.Destroy();
 
-    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
